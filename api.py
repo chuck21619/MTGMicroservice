c7b08616eb532938997318b619f83d4a98391ca8
Charles Johnston <charles.ward.johnston@gmail.com>
2025-06-10 17:51:37 -0400

    add proof of concept code for setting and getting decision tree model from postgresql database

--- api.py
+++ api.py
@@ -4,6 +4,9 @@ from data_generation import *
 from preprocessing import *
 from model import *
 from sklearn.metrics import accuracy_score
+import psycopg2
+import pickle
+
 
 app = FastAPI()
 
@@ -23,7 +26,46 @@ async def predict(request: Request):
 
     x_player, y_player, le_input_players, le_target_players, x_deck, y_deck, le_input_decks, le_target_decks = encode_data(df)
     model, combined_features = train_model(x_player, y_player, le_input_players, le_target_players, x_deck, y_deck, le_input_decks, le_target_decks)
-    meta_predictions = model.predict(combined_features)
+
+    #connecting to database
+    connection = psycopg2.connect("postgresql://postgres:notastupidpassword@localhost:5432/my_local_db?sslmode=disable")
+    current = connection.cursor()
+
+    #save model to database
+    # model_bytes = pickle.dumps(model)
+    # current.execute("UPDATE users SET decision_tree = %s WHERE id = %s", (psycopg2.Binary(model_bytes), 3))
+    # connection.commit()
+    # current.close()
+    # connection.close()
+
+
+    #get model from database
+    current.execute("SELECT decision_tree FROM users WHERE id = %s", (3,))
+    row = current.fetchone()
+    if row and row[0]:
+        model_bytes = row[0]
+        clf = pickle.loads(model_bytes)
+        print("Model loaded successfully.")
+
+
+
+    current.execute("SELECT email FROM users WHERE id = 4")
+    result = current.fetchone()
+    if result:
+        print("email: ", result[0])
+    else:
+        print("shit fucked up")
+
+    current.close()
+    connection.close()
+
+    ## test connection to database. just read some field from it
+    ## so goose migrations to add columns for decision_tree and neural_network
+
+    #get database connection string from environment variables
+    #save model to database
+
+    meta_predictions = clf.predict(combined_features)
     accuracy = accuracy_score(y_player, meta_predictions)
     print("accuracy:", accuracy)
 
